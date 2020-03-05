6a55ffda9e8a954a00c8ef32e654f3e2a54eafa0
SergeyZyukov <plumber@yandex.ru>
2020-03-05 18:16:31 +0300

    Second commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
-@import url(./)
+const popupOpen = document.querySelector('.user-info__photo');
+const popup = document.querySelector('.popup');
+const popupClose = document.querySelector('.popup__close')
+console.log('popup', popupOpen);
+
+const  open = () => popup.classList.add('popup_is-opened');
+  
+const  close = ()=> popup.classList.remove('popup_is-opened');    
+  
+popupOpen.addEventListener('click', open);
+popupClose.addEventListener('click', close)
